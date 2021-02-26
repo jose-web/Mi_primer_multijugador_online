1a3954168cef82883b76da1b6a77453bf982cb82
jose <delossantosramosjose@gmail.com>
2021-02-26 23:35:31 +0100

    Se ha añadido la conexión a la red y se ha añadido el script global

--- Lobby/Lobby.gd
+++ Lobby/Lobby.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
+const MAX_PLAYERS = 2
+const PORT = 6969
+
 func _ready():
-	pass # Replace with function body.
+	get_tree().connect("network_peer_connected", self, "_player_connected")
 
 
 func _on_Multiplayer_pressed():
-	pass # Replace with function body.
+	var net = NetworkedMultiplayerENet.new()
+	net.create_server(PORT, MAX_PLAYERS)
+	get_tree().set_network_peer(net)
 
 
 func _on_Join_pressed():
-	pass # Replace with function body.
+	var net = NetworkedMultiplayerENet.new()
+	net.create_client("127.0.0.1", PORT)
+	get_tree().set_network_peer(net)
+
+func _player_connected(id):
+	pass
